1854d374438bc38312edc02d2f16aafb0bda8474
Ivan Leben <ivan.leben@gmail.com>
2010-10-24 14:25:38 +1100

    Fixed bug where obstacles were initialised on every pathfind request. Fixed bug where default grid of 50,50 might be used for some requests instead of the grid of proper size.

--- RACT-JACKAGT-AOPD10/src/rmit/ai/clima/jackagt/PathGrid.java
+++ RACT-JACKAGT-AOPD10/src/rmit/ai/clima/jackagt/PathGrid.java
@@ -8,67 +8,36 @@ public class PathGrid
 
    public int width;
    public int height;
-   public boolean firstRequest;
+   public boolean gotObstacles;
    PathNode [] grid;
 
 
-   public static PathGrid initInstance(int width, int height)
+   public static PathGrid InitInstance(int width, int height)
    {
       if(instance == null)
       {
-         instance = new PathGrid( width, height );
-         instance.firstRequest = true;
          System.out.println("Instantiating the grid");
+         instance = new PathGrid( width, height );
       }
       return instance;
    }
-   public static PathGrid getInstance()
+   
+   public static PathGrid GetInstance() throws Exception
    {
-      if(instance == null)
-         instance = new PathGrid( 50 , 50);
+      if(instance == null) throw new Exception("Trying to access uninstantiated PathGrid!!!");
       return instance;
    }
 
-   public static void Release()
+   public static void ReleaseInstance()
    {
       instance = null;
-   }
-
-   public void Reset()
-   {
-      this.firstRequest = true;
-      for (int x=0; x<this.width; ++x) {
-         for (int y=0; y<this.height; ++y) {
-
-            PathNode node =  grid[y * width + x]; 
-            node.obstacle = false;
-            node.visited = false;
-            node.f = 0;
-            node.g = 0;
-            node.h = 0;
-         }
-      }
-
-   }
-
-   public void Clean()
-   {
-      for (int x=0; x<this.width; ++x) {
-         for (int y=0; y<this.height; ++y) {
-
-            PathNode node =  grid[y * width + x]; 
-            node.visited = false;
-            node.f = 0;
-            node.g = 0;
-            node.h = 0;
-         }
-      }
    }
 
    public PathGrid (int width, int height)
    {
       this.width = width;
       this.height = height;
+      this.gotObstacles = false;
       grid = new PathNode[ width * height ];
 
       for (int x=0; x<width; ++x) {
@@ -81,15 +50,45 @@ public class PathGrid
          }
       }
    }
-
-   public PathNode getNode (int x, int y)
+   
+   public void clearObstacles()
    {
-      return grid[ y * width + x ];
+      this.gotObstacles = false;
+      for (int x=0; x<this.width; ++x) {
+         for (int y=0; y<this.height; ++y) {
+
+            PathNode node = getNode(x,y);
+            node.obstacle = false;
+            node.visited = false;
+            node.f = 0;
+            node.g = 0;
+            node.h = 0;
+         }
+      }
+   }
+
+   public void clearNodes()
+   {
+      for (int x=0; x<this.width; ++x) {
+         for (int y=0; y<this.height; ++y) {
+
+            PathNode node = getNode(x,y);
+            node.visited = false;
+            node.f = 0;
+            node.g = 0;
+            node.h = 0;
+         }
+      }
    }
 
    public PathNode getNode (GridPoint pt)
    {
       return getNode( pt.x, pt.y );
+   }
+   
+   public PathNode getNode (int x, int y)
+   {
+      return grid[ y * width + x ];
    }
 }
 
